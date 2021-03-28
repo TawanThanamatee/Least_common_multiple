b954509cb62fffef354479a897d751ee5ad3a525
LAPTOP-1M8RSRLB\Pai <tawan.th@mail.kmutt.ac.th>
2021-03-28 12:47:18 +0700

    ส่งข้อสอบ

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Least_common_multiple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, x, y;
+            Console.Write("Input a : ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Inpur b : ");
+            b = int.Parse(Console.ReadLine());
+            x = a;
+            y = b;
+            bool l = false;
+            while (l == false)
+            { 
+                if (x == y)
+                {
+                    Console.WriteLine("Output x : " + x);
+                    l = true;
+                }
+                else if (x < y)
+                {
+                    x = x + a;
+                }
+                else
+                    y = y + b;
+            }
+        }
+    }
+}
